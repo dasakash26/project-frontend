42b7c82b2ffb1f42974a8fba2efb6b11cec3d5c2
Asmit Deb <47671715+asmitdeb@users.noreply.github.com>
2024-11-21 03:48:24 +0530

    modifies routes, landing page route

--- src/App.tsx
+++ src/App.tsx
@@ -14,20 +14,23 @@ import Negotiation from "./pages/Negotiation.tsx";
 import Settings from "./pages/Settings.tsx";
 import Notification from "./components/Notification.tsx";
 import NotFound from "./pages/NotFound.tsx";
+import LandingPage from "./pages/LandingPage.tsx";
+
 const App = () => {
     const router = createBrowserRouter(
         createRoutesFromElements(
           <>
-          <Route path="/" element={<Layout />}>   
-            <Route path="dashboard" element={<Dashboard />} />
+          <Route path= "/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Layout />}>   
+            <Route index element={<Dashboard />} />
             <Route path="search" element={<Search />} />
             <Route path="create-offer" element={<CreateOffer />} />
             <Route path="negotiations" element={<Negotiation />} />
             <Route path="settings" element={<Settings />} />
             <Route path="notifications" element={<Notification />} />
-            <Route path="*" element={<NotFound />}/>
           </Route>
           <Route path="login" element = {<LoginPage />} />
+          <Route path="*" element={<NotFound />}/>
           </>
         )
       );
