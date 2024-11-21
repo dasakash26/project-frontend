603736db40b63e27e901f753f971c630e06a7d65
Asmit Deb <47671715+asmitdeb@users.noreply.github.com>
2024-11-22 02:26:41 +0530

    error in App.tsx routes

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,8 @@ const App = () => {
         createRoutesFromElements(
           <>
           <Route path= "/" element={<LandingPage />} />
-          <Route path="/dashboard" element={<Layout />}>   
-            <Route index element={<Dashboard />} />
+          <Route path= "" element={<Layout />}>
+            <Route path="dashboard" element={<Dashboard />} />
             <Route path="search" element={<Search />} />
             <Route path="create-offer" element={<CreateOffer />} />
             <Route path="negotiations" element={<Negotiation />} />
